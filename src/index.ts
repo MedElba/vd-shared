c2b19fe5e77295df9152bc8cdbf8d8211b593de7
MedElba <medelbayoumi@gmail.com>
2021-12-12 22:44:01 +0100

    user + blog

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from "./blogs/blogs-dto";
 export * from "./blogs/create-blogs.dto";
 export * from "./blogs/update-blogs.dto";
+export * from "./user/user-dto";
+export * from "./user/UserCreateDto";
+export * from "./user/UserUpdateDto";
