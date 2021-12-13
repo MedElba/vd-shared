e422d426f94205bd2a397787a8b91b8dae308794
MedElba <medelbayoumi@gmail.com>
2021-12-13 01:15:27 +0100

    index on root

---
+++ index.ts
@@ -0,0 +1,6 @@
+export * from "./src/blogs/blogs-dto";
+export * from "./src/blogs/create-blogs.dto";
+export * from "./src/blogs/update-blogs.dto";
+export * from "./src/user/user-dto";
+export * from "./src/user/UserCreateDto";
+export * from "./src/user/UserUpdateDto";
